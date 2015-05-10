d84bd7ec2bf355df5f2eaf57abccf32b35a86de8
Shakeel Mohamed <smohamed@splunk.com>
2015-05-10 00:28:23 -0700

    Switched to the official youtube iframe API - also some bug fixes & UI enhancements

--- js/everything.js
+++ js/everything.js
@@ -1,3 +1,88 @@
+/**
+ * YouTube iframe API required setup
+ */
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+var hasError = false;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '30',
+        width: '400',
+        // Parse the querystring and populate the video when loading the page
+        videoId: getCurrentVideoID(),
+        playerVars: { 'autoplay': 1, 'cc_load_policy': 0},
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': function onPlayerStateChange(event) {
+                // Uncomment for debugging
+                // console.log("State changed to " + event.data);
+            },
+            'onError': function(event) {
+                var message = "Got an unknown error, check the JS console.";
+                var verboseMessage = message;
+                hasError = true;
+
+                // Handle the different error codes
+                switch(event.data) {
+                    case 2:
+                        verboseMessage = "The request contains an invalid parameter value. For example, this error occurs if you specify a video ID that does not have 11 characters, or if the video ID contains invalid characters, such as exclamation points or asterisks.";
+                        message = "looks like an invalid video ID";
+                        break;
+                    case 5:
+                        verboseMessage = "The requested content cannot be played in an HTML5 player or another error related to the HTML5 player has occurred.";
+                        message = "we can't play that video here, or something is wrong with YouTube's iframe API";
+                        break;
+                    case 100:
+                        verboseMessage = "The video requested was not found. This error occurs when a video has been removed (for any reason) or has been marked as private.";
+                        message = "we can't find that video, it might be private or removed";
+                        break;
+                    case 101:
+                        verboseMessage = "The owner of the requested video does not allow it to be played in embedded players.";
+                        message = "the video owner won't allow us to play that video";
+                        break;
+                    case 150:
+                        verboseMessage = "This error is the same as 101. It's just a 101 error in disguise!";
+                        message = "the video owner won't allow us to play that video";
+                        break;
+                }
+
+                // Update the UI w/ error
+                $("#zen-video-error").text("ERROR: " + message);
+
+
+                // Log debug info
+                console.log("Verbose debug error message: ", verboseMessage);
+            }
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    // Only play the video if it's actually there
+    if (getCurrentVideoID()) {
+        $("#zen-video-error").text("");
+        event.target.playVideo();
+        $("#zen-video-title").text(player.getVideoData().title);
+    }
+    else {
+        // Clear the now playing text
+        $("#zen-video-title").text("");   
+    }
+}
+
+function stopVideo() {
+    player.stopVideo();
+}
+
+
+/**
+ * Zen YouTube Audio Player functions
+ */
+
 function getParameterByName(url, name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
@@ -11,18 +96,6 @@ function getCurrentVideoID() {
         return parseYoutubeVideoID(v);
     }
     return v;
-}
-
-function makeYoutubeEmbedURL(videoID) {
-    var head = "http://www.youtube.com/embed/";
-    var tail = "?enablejsapi=1&origin=http://shakeelmohamed.com&autoplay=1";
-    if (videoID) {
-        return head + videoID + tail;
-    }
-    else {
-        // Swallow error
-        console.log("The videoID in makeYoutubeEmbedURL was bad");
-    }
 }
 
 function makeListenURL(videoID) {
@@ -40,10 +113,15 @@ function parseYoutubeVideoID(url) {
     var shortUrlDomain = "youtu.be";
     var longUrlDomain = "youtube.com";
 
-    if (url) {
+    if (url && url.length > 0) {
+        console.log(url);
         // youtube.com format
         if (url.indexOf(longUrlDomain) !== -1) {
-             videoID = getParameterByName(url, "v");
+            videoID = getParameterByName(url, "v");
+            // If the URL had 2 v parameters, try parsing the second (usually when ?v=someurl&v=xyz)
+            if (videoID === "") {
+                videoID = getParameterByName(window.location.href.substring(window.location.href.indexOf(url)), "v");
+            }
         }
         // youtu.be format
         else if (url.indexOf(shortUrlDomain) !== -1) {
@@ -55,23 +133,29 @@ function parseYoutubeVideoID(url) {
         else {
             videoID = url;
         }
+
+        var slashPos = videoID.indexOf("/");
+        // We found a slash in the video ID (ex: real id is ABC123, but saw ABC123/zen)
+        // So, only keep what's before the slash
+        if (slashPos !== -1) {
+            videoID = videoID.substring(0, slashPos);
+        }
         return videoID;
     }
     alert("Failed to parse the video ID.");
 }
 
 $(function() {
-    // Parse the querystring and populate the video when loading the page
+    // Preload the form from the URL
     var currentVideoID = getCurrentVideoID();
     if (currentVideoID) {
-        $("#player").attr("src", makeYoutubeEmbedURL(currentVideoID));
-        $("#query").attr("value", currentVideoID);
+        $("#v").attr("value", currentVideoID);
     }
 
     // Handle form submission
     $("#form").submit(function(event) {
         event.preventDefault();
-        var formValue = $("#query").val();
+        var formValue = $("#v").val();
         if (formValue) {
             var videoID = parseYoutubeVideoID(formValue);
             window.location.href = makeListenURL(videoID);
