f7155d3d90c9eb03155cbe86b65bf597022af7b6
Shakeel Mohamed <smohamed@splunk.com>
2015-05-10 21:28:49 -0700

    add some HTML validation tests

--- test/index.js
+++ test/index.js
@@ -5,48 +5,46 @@ var path = require("path");
 var browser = new Browser();
 
 var dir = path.join(__dirname, "..");
-var indexHTMLURL = "file://" + dir + "/index.html";
+// TODO: tests may break on Windows because of this magic
+var indexHTMLURL = "file:" + dir + "/index.html";
 
-// TODO: refactor so visit is only called once; these are passing trivially
-describe("Validate file:", function (done) {
-    describe("elements", function () {
-        it("should have required HTML elements", function () {
-            browser.visit(indexHTMLURL, function () {
-                browser.assert.element("html");
-                browser.assert.element("head");
-                browser.assert.element("body");
-                browser.assert.element("title");
+describe("Tests", function () {
+    describe("Splash page", function () {
+        it("should have required HTML elements", function (done) {
+            browser.visit(indexHTMLURL, function (e) {
+                // 404 for Google Analytics is expected locally
+                assert.equal(e.message, "Server returned status code 404 from file://www.google-analytics.com/analytics.js");
+                assert.ok(browser.query("html"), "Couldn't find <html>, wow!");
+                assert.ok(browser.query("head"), "Couldn't find <head>, wow!");
+                assert.ok(browser.query("body"), "Couldn't find <body>, wow!");
+                assert.ok(browser.query("title"), "Couldn't find <title>, wow!");
                 done();
             });
         });
-        it("should have expected metadata", function () {
-            browser.visit(indexHTMLURL, function () {
-                browser.assert.text("title", "Zen YouTube Audio Player");
-                done();
-            });
+        it("should have expected metadata", function (done) {
+            assert.equal(browser.url, indexHTMLURL);
+            assert.equal(browser.query("title").text, "Zen YouTube Audio Player");
+            done();
         });
-        it("should have expected elements", function () {
-            browser.visit(indexHTMLURL, function () {
-                browser.assert.element("#hero");
-                browser.assert.element("#title");
-                browser.assert.element("#form");
-                browser.assert.element("#demo");
-                browser.assert.element("#submit");
-                browser.assert.element("#zen-audio-error");
-                browser.assert.element("#zen-audio-title");
-                browser.assert.element("#player");
-                done();
-            });
+        it("should have expected elements", function (done) {
+            assert.ok(browser.query("#hero"), "Couldn't find #hero");
+            assert.ok(browser.query("#title"), "Couldn't find #title");
+            assert.ok(browser.query("#form"), "Couldn't find #form");
+            assert.ok(browser.query("#demo"), "Couldn't find #demo");
+            assert.ok(browser.query("#submit"), "Couldn't find #submit");
+            assert.ok(browser.query("#zen-video-error"), "Couldn't find #zen-video-error");
+            assert.ok(browser.query("#zen-video-title"), "Couldn't find #zen-video-title");
+            assert.ok(browser.query("#player"), "Couldn't find #player");
+            done();
         });
-        it("should not have strange HTML elements", function () {
-            browser.visit(indexHTMLURL, function () {
-                browser.assert.element("chicken");
-                browser.assert.element("soup");
-                browser.assert.element("for");
-                browser.assert.element("the");
-                browser.assert.element("code");
-                done();
-            });
+        it("should not have strange HTML elements", function (done) {
+            // These are more of a sanity check than anything else
+            assert.ok(!browser.query("chicken"), "Found unexpected <chicken> element");
+            assert.ok(!browser.query("soup"), "Found unexpected <soup> element");
+            assert.ok(!browser.query("for"), "Found unexpected <for> element");
+            assert.ok(!browser.query("the"), "Found unexpected <the> element");
+            assert.ok(!browser.query("code"), "Found unexpected <code> element");
+            done();
         });
     });
 });
