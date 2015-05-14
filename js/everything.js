dc4e669cb729a213cc8a6be882516603ef9e63cf
Shakeel Mohamed <smohamed@splunk.com>
2015-05-13 22:04:28 -0700

    Created new GA account. Closes #12

--- js/everything.js
+++ js/everything.js
@@ -177,6 +177,5 @@ $(function() {
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-36907845-2', 'shakeelmohamed.com');
-ga('require', 'displayfeatures');
+ga('create', 'UA-62983413-1', 'auto');
 ga('send', 'pageview');
