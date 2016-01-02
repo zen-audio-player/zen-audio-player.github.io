c0ed80f4598285643a3c159c76ea73c683e78e9c
abid <sk.abidhussain@gmail.com>
2016-01-02 19:01:08 +0530

    css issues fix, updated screenshot, hide the player on error

--- js/everything.js
+++ js/everything.js
@@ -243,6 +243,7 @@ function onPlayerReady(event) {
 function showErrorMessage(message) {
     $("#zen-video-error").text("ERROR: " + message);
     $("#zen-video-error").show();
+    $("#audioplayer").hide();
 }
 
 function hideErrorMessage() {
@@ -401,7 +402,6 @@ $(function() {
             ga("send", "event", "demo", "already had video ID in URL");
         }
     });
-
     // Initialize volume slider
     $("#volume").slider({
         min: 0,
