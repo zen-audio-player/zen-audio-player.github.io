d43d6ba427102657779db3e9d2c2c09a0da5717d
Pranav Nair <pranavnair97@gmail.com>
2016-06-10 22:19:40 +0530

    Correcting styling errors

--- js/everything.js
+++ js/everything.js
@@ -473,11 +473,11 @@ $(function() {
             );
         }
     }
-    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) )
+    if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) )
     {
-      $("#container").ready(function(){
-        $("#container").html("<p></p>");
-        $("#change-mobile").html("Whoops! Sorry , we don't support mobile devices.");
+        $("#container").ready(function() {
+          $("#container").html("<p></p>");
+          $("#change-mobile").html("Whoops! Sorry , we don't support mobile devices.");
       });
     }
     // Autocomplete with youtube suggested queries
