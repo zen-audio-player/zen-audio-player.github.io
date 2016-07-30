2546a11e067e36a184bf53598c7a4404e0fa5336
Iakov Boyko <selfishman@Iakovs-MBP.home>
2016-07-29 23:23:57 -0400

    Icons showing in search results cleaned up code

--- js/everything.js
+++ js/everything.js
@@ -499,8 +499,8 @@ $(function() {
                     var end = "</a></h4></li>";
                     $.each(data.items, function(index, result) {
                         var thumbTag = "<img style='float:left; margin: auto' src='http://img.youtube.com/vi/" + result.id.videoId + "/1.jpg'>";
-	                    $("#search-results ul").append(start + result.id.videoId + ">" + thumbTag + result.snippet.title  + end);
-                    });
+                        $("#search-results ul").append(start + result.id.videoId + ">" + thumbTag + result.snippet.title + end);
+                        });
                 },
                 function(jqXHR, textStatus, errorThrown) {
                     logError(jqXHR, textStatus, errorThrown, "Search error");
