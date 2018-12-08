5098792de89281d65001fc037e3b7bb03717ecb1
Joanie Davis <joanie.davis@atomicobject.com>
2018-12-08 10:09:48 -0500

    Add test to ensure description is printed

--- test/demo.js
+++ test/demo.js
@@ -48,4 +48,32 @@ describe("Demo", function () {
             done();
         });
     });
+    it("should show the description when the 'Show Description' button is clicked", function (done) {
+        var oldUrl = browser.location.href;
+        browser.click("#demo", function() {
+            // Make sure the URL changed
+            assert.notEqual(oldUrl, browser.location.href);
+            // Check for any of the demo videos ID in the URL
+            var demos = [
+                "koJv-j1usoI", // The Glitch Mob - Starve the Ego, Feed the Soul
+                "EBerFisqduk", // Cazzette - Together (Lost Kings Remix)
+                "jxKjOOR9sPU", // The Temper Trap - Sweet Disposition
+                "03O2yKUgrKw"  // Mike Mago & Dragonette - Outlines
+            ];
+            assert.notEqual(demos.indexOf(getParameterByName(browser.location.search, "v")), -1);
+            // Check for any of the demo videos ID in the textbox
+            assert.notEqual(demos.indexOf(browser.query("#v").value), -1);
+
+            // TODO: once upon a time, using browser.evaluate("player") would give meaningful
+            //     : info. But there's a race condition where sometimes the player object isn't ready yet...?
+            //     : looks like can't rely on global variables.
+            // TODO: How do we inspect the player object (title, etc.)?
+            browser.assert.element(".plyr");
+            assert.ok(browser.evaluate("window.plyrPlayer"));
+            browser.assert.text("#toggleDescription", "Show Description");
+            browser.assert.text("#zen-error", "");
+            assert.notEqual(true, browser.assert.text("#zen-video-description", ""));
+            done();
+        });
+    });
 });
