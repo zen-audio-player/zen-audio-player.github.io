3eb8fb49239186a12c8cf7cb1fa3f9b46d1035d0
Shakeel Mohamed <contact@shakeel.xyz>
2021-10-16 22:23:03 -0700

    ignore gtag var

--- js/everything.js
+++ js/everything.js
@@ -1,4 +1,4 @@
-/* global URI getSearchResults, getAutocompleteSuggestions, parseYoutubeVideoID, getYouTubeVideoDescription */
+/* global gtag, URI, getSearchResults, getAutocompleteSuggestions, parseYoutubeVideoID, getYouTubeVideoDescription */
 
 var keyCodes = {
     SPACEBAR: 32
