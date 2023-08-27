b075b623a23f45e281446b04a965d0cd40221f78
Shakeel Mohamed <contact@shakeel.xyz>
2023-08-27 00:06:14 -0700

    cleanup style issues

--- test/form.js
+++ test/form.js
@@ -5,22 +5,21 @@ const puppeteer = require("puppeteer");
 const indexHTMLURL = "file://" + path.join(__dirname, "..", "index.html");
 
 (async () => {
-
     before(async function() {
         global.browser = await puppeteer.launch();
     });
 
-    describe('hehe', async function() {
-        it('hi', async function() {
+    describe("hehe", async function() {
+        it("hi", async function() {
             const page = await browser.newPage();
             await page.goto(indexHTMLURL);
 
-            const zenError = await page.waitForSelector('#zen-error');
+            const zenError = await page.waitForSelector("#zen-error");
             let errorValue = await zenError.evaluate(el => el.textContent);
-            assert.equal(errorValue, '');
+            assert.equal(errorValue, "");
 
-            await page.type('#v', 'absolute rubbish');
-            await page.click('#submit');
+            await page.type("#v", "absolute rubbish");
+            await page.click("#submit");
 
             errorValue = await zenError.evaluate(el => el.textContent);
             assert.equal(errorValue, "ERROR: Skipping video lookup request as we're running the site locally.");
