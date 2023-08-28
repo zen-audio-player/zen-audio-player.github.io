52582b958421b5756eadb4c6ba7b97aafd3cf8ad
Shakeel Mohamed <contact@shakeel.xyz>
2023-08-28 01:46:09 -0700

    finish test conversion

--- test/page_structure.js
+++ test/page_structure.js
@@ -38,10 +38,10 @@ async function getProperty(page, selector, property) {
 
             assert.equal(await getProperty(page, "meta ~ meta ~ meta ~ meta", "name"), "viewport");
             assert.equal(await getProperty(page, "meta ~ meta ~ meta ~ meta", "content"), "width=device-width, initial-scale=1");
-            
+
             assert.equal(await getProperty(page, "meta ~ meta ~ meta ~ meta ~ meta", "name"), "google-site-verification");
             assert.equal(await getProperty(page, "meta ~ meta ~ meta ~ meta ~ meta", "content"), "D3SjNR3tmNYOusESQijh_oH5SGmU9QsAIVwlqizwRBU");
-            
+
             assert.equal(await getProperty(page, "title", "text"), "Zen Audio Player");
         });
 
@@ -60,49 +60,62 @@ async function getProperty(page, selector, property) {
             assert.equal(await getProperty(page, "link ~ link", "type"), "image/x-icon");
             assert.equal(await getProperty(page, "link ~ link", "rel"), "icon");
         });
-        //     it("should have CSS files configured correctly", function () {
-        //         var preloadStylesheet = "preload stylesheet";
-        //         assert.equal(browser.query("link ~ link ~ link").rel, preloadStylesheet);
-        //         assert.ok(browser.query("link ~ link ~ link").href.endsWith("bower_components/primer-css/css/primer.css"));
+        it("should have CSS files configured correctly", async function () {
+            const page = await browser.newPage();
+            await page.goto(indexHTMLURL);
 
-        //         assert.equal(browser.query("link ~ link ~ link ~ link").rel, preloadStylesheet);
-        //         assert.ok(browser.query("link ~ link ~ link ~ link").href.endsWith("bower_components/font-awesome/css/font-awesome.min.css"));
+            const preloadStylesheet = "preload stylesheet";
 
-        //         assert.equal(browser.query("link ~ link ~ link ~ link ~ link").rel, preloadStylesheet);
-        //         assert.ok(browser.query("link ~ link ~ link ~ link ~ link").href.endsWith("bower_components/plyr/dist/plyr.css"));
+            assert.equal(await getProperty(page, "link ~ link ~ link", "rel"), preloadStylesheet);
+            assert.ok((await getProperty(page, "link ~ link ~ link", "href")).endsWith("bower_components/primer-css/css/primer.css"));
 
-        //         assert.equal(browser.query("link ~ link ~ link ~ link ~ link ~ link").rel, preloadStylesheet);
-        //         assert.ok(browser.query("link ~ link ~ link ~ link ~ link ~ link").href.endsWith("css/styles.css"));
-        //     });
-        //     it("should have logo configured correctly", function () {
-        //         var imgFolderPath = path.join(__filename, "..", "..", "img") + path.sep;
-        //         assert.ok(fs.existsSync(imgFolderPath) + "zen-audio-player-113.png");
-        //         assert.ok(fs.existsSync(imgFolderPath) + "zen-audio-player-453.png");
-        //         assert.ok(fs.existsSync(imgFolderPath) + "zen-audio-player-905.png");
-        //         assert.equal(browser.query("header > figure > a").href, "https://zen-audio-player.github.io/");
-        //         assert.ok(browser.query("header > figure > a.zen-logo > img.img-100").src.indexOf("img/zen-audio-player-905.png") !== -1);
-        //         assert.equal(browser.query("header > figure > a.zen-logo > img.img-100").alt, "Zen Audio Player logo");
-        //     });
-        //     it("should have expected elements", function () {
-        //         assert.ok(browser.query("header"), "Couldn't find header");
-        //         assert.ok(browser.query("header > figure"), "Couldn't find <header><figure>");
-        //         assert.ok(browser.query("header > figure > a"), "Couldn't find <header><figure><a>");
-        //         assert.ok(browser.query("header > figure > a.zen-logo > img.img-100"), "Couldn't find <header><figure><a><img>");
-        //         assert.ok(browser.query("#form"), "Couldn't find #form");
-        //         // TODO: validate the rest of the form
-        //         assert.ok(browser.query("#demo"), "Couldn't find #demo");
-        //         assert.ok(browser.query("#submit"), "Couldn't find #submit");
-        //         assert.ok(browser.query("#zen-error"), "Couldn't find #zen-error");
-        //         assert.ok(browser.query("#zen-video-title"), "Couldn't find #zen-video-title");
-        //         assert.ok(browser.query("h3 > a#zen-video-title"), "Couldn't find a h3 > a#zen-video-title");
-        //         assert.ok(browser.query("#audioplayer"), "Couldn't find #audioplayer");
-        //         assert.ok(browser.query("#audioplayer > div.plyr"), "Couldn't find #audioplayer > div.plyr");
+            assert.equal(await getProperty(page, "link ~ link ~ link ~ link", "rel"), preloadStylesheet);
+            assert.ok((await getProperty(page, "link ~ link ~ link ~ link", "href")).endsWith("bower_components/font-awesome/css/font-awesome.min.css"));
 
-        //         assert.ok(browser.query("footer"), "Couldn't find footer");
-        //         assert.ok(browser.query("footer > div.color-grey > p"), "Couldn't find footer > div.color-grey <p>Created by");
-        //         assert.ok(browser.query("footer > div.color-grey > p ~ p"), "Couldn't find footer > div.color-grey<p>Created by...</p><p>");
-        //         assert.ok(browser.query("footer > div.repo-info > p"), "Couldn't find footer > div.repo-info<p>Source available on GitHub...</p><p>");
-        //     });
+            assert.equal(await getProperty(page, "link ~ link ~ link ~ link ~ link", "rel"), preloadStylesheet);
+            assert.ok((await getProperty(page, "link ~ link ~ link ~ link ~ link", "href")).endsWith("bower_components/plyr/dist/plyr.css"));
+
+            assert.equal(await getProperty(page, "link ~ link ~ link ~ link ~ link ~ link", "rel"), preloadStylesheet);
+            assert.ok((await getProperty(page, "link ~ link ~ link ~ link ~ link ~ link", "href")).endsWith("css/styles.css"));
+        });
+        it("should have logo configured correctly", async function () {
+            const page = await browser.newPage();
+            await page.goto(indexHTMLURL);
+
+            const imgFolderPath = path.join(__filename, "..", "..", "img") + path.sep;
+
+            assert.ok(fs.existsSync(imgFolderPath) + "zen-audio-player-113.png");
+            assert.ok(fs.existsSync(imgFolderPath) + "zen-audio-player-453.png");
+            assert.ok(fs.existsSync(imgFolderPath) + "zen-audio-player-905.png");
+
+            assert.equal(await getProperty(page, "header > figure > a", "href"), "https://zen-audio-player.github.io/");
+            assert.ok((await getProperty(page, "header > figure > a.zen-logo > img.img-100", "src")).indexOf("img/zen-audio-player-905.png") !== -1);
+            assert.equal(await getProperty(page, "header > figure > a.zen-logo > img.img-100", "alt"), "Zen Audio Player logo");
+        });
+        it("should have expected elements", async function () {
+            const page = await browser.newPage();
+            await page.goto(indexHTMLURL);
+
+            assert.ok(page.$("header"), "Couldn't find header");
+            assert.ok(page.$("header > figure"), "Couldn't find <header><figure>");
+            assert.ok(page.$("header > figure > a"), "Couldn't find <header><figure><a>");
+            assert.ok(page.$("header > figure > a.zen-logo > img.img-100"), "Couldn't find <header><figure><a><img>");
+            assert.ok(page.$("#form"), "Couldn't find #form");
+
+            // TODO: validate the rest of the form
+            assert.ok(page.$("#demo"), "Couldn't find #demo");
+            assert.ok(page.$("#submit"), "Couldn't find #submit");
+            assert.ok(page.$("#zen-error"), "Couldn't find #zen-error");
+            assert.ok(page.$("#zen-video-title"), "Couldn't find #zen-video-title");
+            assert.ok(page.$("h3 > a#zen-video-title"), "Couldn't find a h3 > a#zen-video-title");
+            assert.ok(page.$("#audioplayer"), "Couldn't find #audioplayer");
+            assert.ok(page.$("#audioplayer > div.plyr"), "Couldn't find #audioplayer > div.plyr");
+
+            assert.ok(page.$("footer"), "Couldn't find footer");
+            assert.ok(page.$("footer > div.color-grey > p"), "Couldn't find footer > div.color-grey <p>Created by");
+            assert.ok(page.$("footer > div.color-grey > p ~ p"), "Couldn't find footer > div.color-grey<p>Created by...</p><p>");
+            assert.ok(page.$("footer > div.repo-info > p"), "Couldn't find footer > div.repo-info<p>Source available on GitHub...</p><p>");
+        });
         it("should not have strange HTML elements", async function () {
             const page = await browser.newPage();
             await page.goto(indexHTMLURL);
